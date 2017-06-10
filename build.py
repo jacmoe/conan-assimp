009817b6d717624c799d602160db4f24839818b5
Jacob Moen <jacmoe.dk@gmail.com>
2017-06-10 20:46:07 +0200

    Create build.py

--- build.py
+++ build.py
@@ -3,4 +3,9 @@ from conan.packager import ConanMultiPackager
 if __name__ == "__main__":
     builder = ConanMultiPackager()
     builder.add_common_builds(pure_c=False)
+    builder.builds = [
+        [settings, options, env_vars, build_requires]
+        for settings, options, env_vars, build_requires in builder.builds
+        if not (os_info.is_linux and settings["arch"] == "x86")
+    ]
     builder.run()
