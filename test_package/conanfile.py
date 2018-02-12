4bf50ed280c95b1945aac0d80ca26bc1750cd5f2
Pedro Vicente <pedrovfer@gmail.com>
2018-02-12 15:52:12 +0000

    Fixing test_package

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -1,14 +1,14 @@
 from conans import ConanFile, CMake
 import os
 
+
 class AssimpTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake"
 
     def build(self):
         cmake = CMake(self)
-        # Current dir is "test_package/build/<build_id>" and CMakeLists.txt is in "test_package"
-        cmake.configure(source_dir=self.conanfile_directory, build_dir="./")
+        cmake.configure()
         cmake.build()
 
     def imports(self):
