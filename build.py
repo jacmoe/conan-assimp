cabc8b3461ea2114d516bf3c371244fe4514b9a8
Pedro Vicente <pedrovfer@gmail.com>
2018-02-12 16:15:57 +0000

    Updated travis configuration

--- build.py
+++ build.py
@@ -1,13 +1,6 @@
 from conan.packager import ConanMultiPackager
-from conans.tools import os_info
-import platform
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(args="--build missing")
-    builder.add_common_builds(pure_c=False)
-    builder.builds = [
-        [settings, options, env_vars, build_requires]
-        for settings, options, env_vars, build_requires in builder.builds
-        if not (os_info.is_linux and settings["arch"] == "x86")
-    ]
+    builder = ConanMultiPackager()
+    builder.add_common_builds(shared_option_name="Assimp:shared", pure_c=False)
     builder.run()
